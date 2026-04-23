8c112d34857be5aca2edc07a3f9789fa64371f8d
Bernhard Haubold <haubold@evolbio.mpg.de>
2026-04-23 16:39:41 +0200

    Updated the tests of fetch to current never

--- fetch/fetch_test.go
+++ fetch/fetch_test.go
@@ -15,30 +15,47 @@ func TestFetch(t *testing.T) {
 	tests = append(tests, test)
 	query := "t=9606"
 	tmpl := "%s/%s/?%s"
-	eURL := fmt.Sprintf(tmpl, url, "accessions", query)
+	eURL := fmt.Sprintf(tmpl, url, "children", query)
 	test = exec.Command("./fetch", eURL)
 	tests = append(tests, test)
-	eURL = fmt.Sprintf(tmpl, url, "children", query)
+	eURL = fmt.Sprintf(tmpl, url, "num_genomes", query)
+	test = exec.Command("./fetch", eURL)
+	tests = append(tests, test)
+	eURL = fmt.Sprintf(tmpl, url, "num_genomes_rec", query)
 	test = exec.Command("./fetch", eURL)
 	tests = append(tests, test)
 	eURL = fmt.Sprintf(tmpl, url, "parent", query)
 	test = exec.Command("./fetch", eURL)
 	tests = append(tests, test)
+	eURL = fmt.Sprintf(tmpl, url, "subtree", query)
+	test = exec.Command("./fetch", eURL)
+	tests = append(tests, test)
 	query = "t=9606,741158,63221"
+	eURL = fmt.Sprintf(tmpl, url, "accessions", query)
+	test = exec.Command("./fetch", eURL)
+	tests = append(tests, test)
 	eURL = fmt.Sprintf(tmpl, url, "mrca", query)
 	test = exec.Command("./fetch", eURL)
 	tests = append(tests, test)
 	eURL = fmt.Sprintf(tmpl, url, "names", query)
 	test = exec.Command("./fetch", eURL)
 	tests = append(tests, test)
+	query = "t=9606,40674"
+	eURL = fmt.Sprintf(tmpl, url, "path", query)
+	test = exec.Command("./fetch", eURL)
+	tests = append(tests, test)
 	eURL = fmt.Sprintf(tmpl, url, "ranks", query)
 	test = exec.Command("./fetch", eURL)
 	tests = append(tests, test)
-	query = "t=Homo sapiens"
-	eURL = fmt.Sprintf(tmpl, url, "taxi", query)
+	eURL = fmt.Sprintf(tmpl, url, "taxa_info", query)
 	test = exec.Command("./fetch", eURL)
 	tests = append(tests, test)
+	query = "t=Homo sapiens"
 	eURL = fmt.Sprintf(tmpl, url, "taxids", query)
+	test = exec.Command("./fetch", eURL)
+	tests = append(tests, test)
+	query += "&n=10&p=1"
+	eURL = fmt.Sprintf(tmpl, url, "taxi", query)
 	test = exec.Command("./fetch", eURL)
 	tests = append(tests, test)
 	query = "a=GCF_000001405.40,GCA_000002115.2"
@@ -50,13 +67,13 @@ func TestFetch(t *testing.T) {
 		if err != nil {
 			t.Error(err)
 		}
-		name := fmt.Sprintf("r%d.txt", i+1)
-		want, err := os.ReadFile(name)
+		f := fmt.Sprintf("r%d.txt", i+1)
+		want, err := os.ReadFile(f)
 		if err != nil {
 			t.Error(err)
 		}
 		if !bytes.Equal(get, want) {
-			t.Errorf("get:\n%s\nwant:\n%s\n", get, want)
+			t.Errorf("%s get:\n%s\nwant:\n%s\n", f, get, want)
 		}
 	}
 }
