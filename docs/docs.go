08b5ab0691c5e46318c984c6478bf392c342eddc
BenBahnsen <ben_bahnsen@web.de>
2026-05-09 19:51:21 +0200

    scrolling to selected header

--- docs/docs.go
+++ docs/docs.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 	"os"
 	"path"
+	"strings"
 )
 
 type Content struct {
@@ -22,6 +23,7 @@ type Endpoint struct {
 	Description   string
 	Example       string
 	RequestType   string
+	Extended      bool
 	PathParams    []Argument
 	QueryParams   []Argument
 	ResponseCodes []ResponseCode
@@ -44,6 +46,20 @@ const (
 	StatusCode500 = "Internal Server Error"
 )
 
+func toCSSId(args ...string) string {
+	noSlash := make([]string, len(args))
+	for i, str := range args {
+		noSlash[i] = strings.ReplaceAll(str, "/", "-")
+	}
+
+	joined := strings.Join(noSlash, "-")
+	if len(joined) > 1 && joined[0] == '-' {
+		joined = joined[1:]
+	}
+
+	return joined
+}
+
 func RegisterRoutes(prefix string) {
 	var err error
 	fmt.Println("Creating template")
@@ -54,6 +70,7 @@ func RegisterRoutes(prefix string) {
 		"sub": func(a, b int) int {
 			return a - b
 		},
+		"toCSSId": toCSSId,
 	})
 
 	fmt.Println("Reading files")
@@ -66,13 +83,15 @@ func RegisterRoutes(prefix string) {
 	}
 
 	fmt.Println("Registering handlers")
-	http.HandleFunc(prefix+"/", func(w http.ResponseWriter, r *http.Request) { defaultHandler(tmpl, w, r) })
+	http.HandleFunc(prefix, func(w http.ResponseWriter, r *http.Request) { defaultHandler(tmpl, w, r) })
 
 	http.Handle("/docs/v2/static/", http.StripPrefix("/docs/v2/static/", http.FileServer(http.Dir("docs/static"))))
 }
 
-func defaultHandler(tmpl *template.Template, w http.ResponseWriter, _ *http.Request) {
+func defaultHandler(tmpl *template.Template, w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Served default")
+
+	fmt.Println(r.URL)
 
 	tmpl.ExecuteTemplate(w, "app.html",
 		&Content{Endpoints: []Endpoint{
@@ -101,6 +120,601 @@ func defaultHandler(tmpl *template.Template, w http.ResponseWriter, _ *http.Requ
 					{Code: 200, Description: StatusCode200},
 					{Code: 400, Description: StatusCode400},
 				},
+				Extended: false,
+			},
+			{
+				Route:       "/children/{tax-id}",
+				Description: "Returns path connection start and end",
+				Example:     "/accessions/278148",
+				RequestType: "GET",
+				PathParams: []Argument{
+					{
+						Name:        "from",
+						Datatype:    "int",
+						Example:     "9234",
+						Description: "the id of the start taxon",
+						Required:    true,
+					},
+					{
+						Name:        "to",
+						Datatype:    "int",
+						Example:     "526",
+						Description: "the id of the end taxon",
+						Required:    true,
+					},
+				},
+				ResponseCodes: []ResponseCode{
+					{Code: 200, Description: StatusCode200},
+					{Code: 400, Description: StatusCode400},
+				},
+				Extended: true,
+			},
+			{
+				Route:       "/parent/{tax-id}",
+				Description: "Returns path connection start and end",
+				Example:     "/accessions/278148",
+				RequestType: "GET",
+				PathParams: []Argument{
+					{
+						Name:        "from",
+						Datatype:    "int",
+						Example:     "9234",
+						Description: "the id of the start taxon",
+						Required:    true,
+					},
+					{
+						Name:        "to",
+						Datatype:    "int",
+						Example:     "526",
+						Description: "the id of the end taxon",
+						Required:    true,
+					},
+				},
+				ResponseCodes: []ResponseCode{
+					{Code: 200, Description: StatusCode200},
+					{Code: 400, Description: StatusCode400},
+				},
+				Extended: false,
+			},
+			{
+				Route:       "/parent21/{tax-id}",
+				Description: "Returns path connection start and end",
+				Example:     "/accessions/278148",
+				RequestType: "GET",
+				PathParams: []Argument{
+					{
+						Name:        "from",
+						Datatype:    "int",
+						Example:     "9234",
+						Description: "the id of the start taxon",
+						Required:    true,
+					},
+					{
+						Name:        "to",
+						Datatype:    "int",
+						Example:     "526",
+						Description: "the id of the end taxon",
+						Required:    true,
+					},
+				},
+				ResponseCodes: []ResponseCode{
+					{Code: 200, Description: StatusCode200},
+					{Code: 400, Description: StatusCode400},
+				},
+				Extended: false,
+			},
+			{
+				Route:       "/parent20/{tax-id}",
+				Description: "Returns path connection start and end",
+				Example:     "/accessions/278148",
+				RequestType: "GET",
+				PathParams: []Argument{
+					{
+						Name:        "from",
+						Datatype:    "int",
+						Example:     "9234",
+						Description: "the id of the start taxon",
+						Required:    true,
+					},
+					{
+						Name:        "to",
+						Datatype:    "int",
+						Example:     "526",
+						Description: "the id of the end taxon",
+						Required:    true,
+					},
+				},
+				ResponseCodes: []ResponseCode{
+					{Code: 200, Description: StatusCode200},
+					{Code: 400, Description: StatusCode400},
+				},
+				Extended: false,
+			},
+			{
+				Route:       "/parent19/{tax-id}",
+				Description: "Returns path connection start and end",
+				Example:     "/accessions/278148",
+				RequestType: "GET",
+				PathParams: []Argument{
+					{
+						Name:        "from",
+						Datatype:    "int",
+						Example:     "9234",
+						Description: "the id of the start taxon",
+						Required:    true,
+					},
+					{
+						Name:        "to",
+						Datatype:    "int",
+						Example:     "526",
+						Description: "the id of the end taxon",
+						Required:    true,
+					},
+				},
+				ResponseCodes: []ResponseCode{
+					{Code: 200, Description: StatusCode200},
+					{Code: 400, Description: StatusCode400},
+				},
+				Extended: false,
+			},
+			{
+				Route:       "/parent18/{tax-id}",
+				Description: "Returns path connection start and end",
+				Example:     "/accessions/278148",
+				RequestType: "GET",
+				PathParams: []Argument{
+					{
+						Name:        "from",
+						Datatype:    "int",
+						Example:     "9234",
+						Description: "the id of the start taxon",
+						Required:    true,
+					},
+					{
+						Name:        "to",
+						Datatype:    "int",
+						Example:     "526",
+						Description: "the id of the end taxon",
+						Required:    true,
+					},
+				},
+				ResponseCodes: []ResponseCode{
+					{Code: 200, Description: StatusCode200},
+					{Code: 400, Description: StatusCode400},
+				},
+				Extended: false,
+			},
+			{
+				Route:       "/parent17/{tax-id}",
+				Description: "Returns path connection start and end",
+				Example:     "/accessions/278148",
+				RequestType: "GET",
+				PathParams: []Argument{
+					{
+						Name:        "from",
+						Datatype:    "int",
+						Example:     "9234",
+						Description: "the id of the start taxon",
+						Required:    true,
+					},
+					{
+						Name:        "to",
+						Datatype:    "int",
+						Example:     "526",
+						Description: "the id of the end taxon",
+						Required:    true,
+					},
+				},
+				ResponseCodes: []ResponseCode{
+					{Code: 200, Description: StatusCode200},
+					{Code: 400, Description: StatusCode400},
+				},
+				Extended: false,
+			},
+			{
+				Route:       "/parent16/{tax-id}",
+				Description: "Returns path connection start and end",
+				Example:     "/accessions/278148",
+				RequestType: "GET",
+				PathParams: []Argument{
+					{
+						Name:        "from",
+						Datatype:    "int",
+						Example:     "9234",
+						Description: "the id of the start taxon",
+						Required:    true,
+					},
+					{
+						Name:        "to",
+						Datatype:    "int",
+						Example:     "526",
+						Description: "the id of the end taxon",
+						Required:    true,
+					},
+				},
+				ResponseCodes: []ResponseCode{
+					{Code: 200, Description: StatusCode200},
+					{Code: 400, Description: StatusCode400},
+				},
+				Extended: false,
+			},
+			{
+				Route:       "/parent15/{tax-id}",
+				Description: "Returns path connection start and end",
+				Example:     "/accessions/278148",
+				RequestType: "GET",
+				PathParams: []Argument{
+					{
+						Name:        "from",
+						Datatype:    "int",
+						Example:     "9234",
+						Description: "the id of the start taxon",
+						Required:    true,
+					},
+					{
+						Name:        "to",
+						Datatype:    "int",
+						Example:     "526",
+						Description: "the id of the end taxon",
+						Required:    true,
+					},
+				},
+				ResponseCodes: []ResponseCode{
+					{Code: 200, Description: StatusCode200},
+					{Code: 400, Description: StatusCode400},
+				},
+				Extended: false,
+			},
+			{
+				Route:       "/parent14/{tax-id}",
+				Description: "Returns path connection start and end",
+				Example:     "/accessions/278148",
+				RequestType: "GET",
+				PathParams: []Argument{
+					{
+						Name:        "from",
+						Datatype:    "int",
+						Example:     "9234",
+						Description: "the id of the start taxon",
+						Required:    true,
+					},
+					{
+						Name:        "to",
+						Datatype:    "int",
+						Example:     "526",
+						Description: "the id of the end taxon",
+						Required:    true,
+					},
+				},
+				ResponseCodes: []ResponseCode{
+					{Code: 200, Description: StatusCode200},
+					{Code: 400, Description: StatusCode400},
+				},
+				Extended: false,
+			},
+			{
+				Route:       "/parent13/{tax-id}",
+				Description: "Returns path connection start and end",
+				Example:     "/accessions/278148",
+				RequestType: "GET",
+				PathParams: []Argument{
+					{
+						Name:        "from",
+						Datatype:    "int",
+						Example:     "9234",
+						Description: "the id of the start taxon",
+						Required:    true,
+					},
+					{
+						Name:        "to",
+						Datatype:    "int",
+						Example:     "526",
+						Description: "the id of the end taxon",
+						Required:    true,
+					},
+				},
+				ResponseCodes: []ResponseCode{
+					{Code: 200, Description: StatusCode200},
+					{Code: 400, Description: StatusCode400},
+				},
+				Extended: false,
+			},
+			{
+				Route:       "/parent12/{tax-id}",
+				Description: "Returns path connection start and end",
+				Example:     "/accessions/278148",
+				RequestType: "GET",
+				PathParams: []Argument{
+					{
+						Name:        "from",
+						Datatype:    "int",
+						Example:     "9234",
+						Description: "the id of the start taxon",
+						Required:    true,
+					},
+					{
+						Name:        "to",
+						Datatype:    "int",
+						Example:     "526",
+						Description: "the id of the end taxon",
+						Required:    true,
+					},
+				},
+				ResponseCodes: []ResponseCode{
+					{Code: 200, Description: StatusCode200},
+					{Code: 400, Description: StatusCode400},
+				},
+				Extended: false,
+			},
+			{
+				Route:       "/parent10/{tax-id}",
+				Description: "Returns path connection start and end",
+				Example:     "/accessions/278148",
+				RequestType: "GET",
+				PathParams: []Argument{
+					{
+						Name:        "from",
+						Datatype:    "int",
+						Example:     "9234",
+						Description: "the id of the start taxon",
+						Required:    true,
+					},
+					{
+						Name:        "to",
+						Datatype:    "int",
+						Example:     "526",
+						Description: "the id of the end taxon",
+						Required:    true,
+					},
+				},
+				ResponseCodes: []ResponseCode{
+					{Code: 200, Description: StatusCode200},
+					{Code: 400, Description: StatusCode400},
+				},
+				Extended: false,
+			},
+			{
+				Route:       "/parent9/{tax-id}",
+				Description: "Returns path connection start and end",
+				Example:     "/accessions/278148",
+				RequestType: "GET",
+				PathParams: []Argument{
+					{
+						Name:        "from",
+						Datatype:    "int",
+						Example:     "9234",
+						Description: "the id of the start taxon",
+						Required:    true,
+					},
+					{
+						Name:        "to",
+						Datatype:    "int",
+						Example:     "526",
+						Description: "the id of the end taxon",
+						Required:    true,
+					},
+				},
+				ResponseCodes: []ResponseCode{
+					{Code: 200, Description: StatusCode200},
+					{Code: 400, Description: StatusCode400},
+				},
+				Extended: false,
+			},
+			{
+				Route:       "/parent8/{tax-id}",
+				Description: "Returns path connection start and end",
+				Example:     "/accessions/278148",
+				RequestType: "GET",
+				PathParams: []Argument{
+					{
+						Name:        "from",
+						Datatype:    "int",
+						Example:     "9234",
+						Description: "the id of the start taxon",
+						Required:    true,
+					},
+					{
+						Name:        "to",
+						Datatype:    "int",
+						Example:     "526",
+						Description: "the id of the end taxon",
+						Required:    true,
+					},
+				},
+				ResponseCodes: []ResponseCode{
+					{Code: 200, Description: StatusCode200},
+					{Code: 400, Description: StatusCode400},
+				},
+				Extended: false,
+			},
+			{
+				Route:       "/parent7/{tax-id}",
+				Description: "Returns path connection start and end",
+				Example:     "/accessions/278148",
+				RequestType: "GET",
+				PathParams: []Argument{
+					{
+						Name:        "from",
+						Datatype:    "int",
+						Example:     "9234",
+						Description: "the id of the start taxon",
+						Required:    true,
+					},
+					{
+						Name:        "to",
+						Datatype:    "int",
+						Example:     "526",
+						Description: "the id of the end taxon",
+						Required:    true,
+					},
+				},
+				ResponseCodes: []ResponseCode{
+					{Code: 200, Description: StatusCode200},
+					{Code: 400, Description: StatusCode400},
+				},
+				Extended: false,
+			},
+			{
+				Route:       "/parent6/{tax-id}",
+				Description: "Returns path connection start and end",
+				Example:     "/accessions/278148",
+				RequestType: "GET",
+				PathParams: []Argument{
+					{
+						Name:        "from",
+						Datatype:    "int",
+						Example:     "9234",
+						Description: "the id of the start taxon",
+						Required:    true,
+					},
+					{
+						Name:        "to",
+						Datatype:    "int",
+						Example:     "526",
+						Description: "the id of the end taxon",
+						Required:    true,
+					},
+				},
+				ResponseCodes: []ResponseCode{
+					{Code: 200, Description: StatusCode200},
+					{Code: 400, Description: StatusCode400},
+				},
+				Extended: false,
+			},
+			{
+				Route:       "/parent5/{tax-id}",
+				Description: "Returns path connection start and end",
+				Example:     "/accessions/278148",
+				RequestType: "GET",
+				PathParams: []Argument{
+					{
+						Name:        "from",
+						Datatype:    "int",
+						Example:     "9234",
+						Description: "the id of the start taxon",
+						Required:    true,
+					},
+					{
+						Name:        "to",
+						Datatype:    "int",
+						Example:     "526",
+						Description: "the id of the end taxon",
+						Required:    true,
+					},
+				},
+				ResponseCodes: []ResponseCode{
+					{Code: 200, Description: StatusCode200},
+					{Code: 400, Description: StatusCode400},
+				},
+				Extended: false,
+			},
+			{
+				Route:       "/parent4/{tax-id}",
+				Description: "Returns path connection start and end",
+				Example:     "/accessions/278148",
+				RequestType: "GET",
+				PathParams: []Argument{
+					{
+						Name:        "from",
+						Datatype:    "int",
+						Example:     "9234",
+						Description: "the id of the start taxon",
+						Required:    true,
+					},
+					{
+						Name:        "to",
+						Datatype:    "int",
+						Example:     "526",
+						Description: "the id of the end taxon",
+						Required:    true,
+					},
+				},
+				ResponseCodes: []ResponseCode{
+					{Code: 200, Description: StatusCode200},
+					{Code: 400, Description: StatusCode400},
+				},
+				Extended: false,
+			},
+			{
+				Route:       "/parent3/{tax-id}",
+				Description: "Returns path connection start and end",
+				Example:     "/accessions/278148",
+				RequestType: "GET",
+				PathParams: []Argument{
+					{
+						Name:        "from",
+						Datatype:    "int",
+						Example:     "9234",
+						Description: "the id of the start taxon",
+						Required:    true,
+					},
+					{
+						Name:        "to",
+						Datatype:    "int",
+						Example:     "526",
+						Description: "the id of the end taxon",
+						Required:    true,
+					},
+				},
+				ResponseCodes: []ResponseCode{
+					{Code: 200, Description: StatusCode200},
+					{Code: 400, Description: StatusCode400},
+				},
+				Extended: false,
+			},
+			{
+				Route:       "/parent2/{tax-id}",
+				Description: "Returns path connection start and end",
+				Example:     "/accessions/278148",
+				RequestType: "GET",
+				PathParams: []Argument{
+					{
+						Name:        "from",
+						Datatype:    "int",
+						Example:     "9234",
+						Description: "the id of the start taxon",
+						Required:    true,
+					},
+					{
+						Name:        "to",
+						Datatype:    "int",
+						Example:     "526",
+						Description: "the id of the end taxon",
+						Required:    true,
+					},
+				},
+				ResponseCodes: []ResponseCode{
+					{Code: 200, Description: StatusCode200},
+					{Code: 400, Description: StatusCode400},
+				},
+				Extended: false,
+			},
+			{
+				Route:       "/parent1/{tax-id}",
+				Description: "Returns path connection start and end",
+				Example:     "/accessions/278148",
+				RequestType: "GET",
+				PathParams: []Argument{
+					{
+						Name:        "from",
+						Datatype:    "int",
+						Example:     "9234",
+						Description: "the id of the start taxon",
+						Required:    true,
+					},
+					{
+						Name:        "to",
+						Datatype:    "int",
+						Example:     "526",
+						Description: "the id of the end taxon",
+						Required:    true,
+					},
+				},
+				ResponseCodes: []ResponseCode{
+					{Code: 200, Description: StatusCode200},
+					{Code: 400, Description: StatusCode400},
+				},
+				Extended: false,
 			},
 		}})
 }
