eef3a1bbdc4294d9e2dcd15d3ccd6cc0b058ea4e
Ben Bahnsen <ben_bahnsen@web.de>
2026-05-15 10:55:38 +0200

    added files and directories to gitignore

--- api/v2/api.go
+++ api/v2/api.go
@@ -1,1 +1,60 @@
 package apiv2
+
+import "net/http"
+
+type Accession struct {
+	Accession string `json:"accession"`
+	Level     string `json:"level"`
+}
+
+type GenomeCount struct {
+	Level string `json:"level"`
+	Count int    `json:"count"`
+}
+
+type Image struct {
+	Id          int    `json:"id"`
+	Url         string `json:"url"`
+	Attribution string `json:"attribution"`
+}
+
+type Rank struct {
+	TaxId int    `json:"tax_id"`
+	Rank  string `json:"rank"`
+}
+
+type TaxonAccessions struct {
+	TaxId      int         `json:"tax_id"`
+	Accessions []Accession `json:"accessions"`
+}
+
+type TaxId struct {
+	TaxId int `json:"tax_id"`
+}
+
+type TaxInfo struct {
+	TaxId          int           `json:"tax_id"`
+	Parent         int           `json:"parent"`
+	IsLeaf         int           `json:"is_leaf"`
+	Name           string        `json:"name"`
+	CommonName     string        `json:"common_name"`
+	Rank           string        `json:"rank"`
+	RawGenomeCount []GenomeCount `json:"raw_genome_count"`
+	RecGenomeCount []GenomeCount `json:"rec_genome_count"`
+	Images         []Image       `json:"images"`
+}
+
+type Taxon struct {
+	TaxId      int    `json:"tax_id"`
+	Parent     int    `json:"parent"`
+	Name       string `json:"name"`
+	CommonName string `json:"common_name"`
+}
+
+type TaxonName struct {
+	TaxId      int    `json:"tax_id"`
+	Name       string `json:"name"`
+	CommonName string `json:"common_name"`
+}
+
+func v(p string, fn func(func(http.ResponseWriter, *http.Request, ...any)), sdfsdf int)
--- main.go
+++ main.go
@@ -14,7 +14,8 @@ func main() {
 		http.Redirect(w, r, "/docs/api/v2", http.StatusSeeOther)
 	})
 
-	fmt.Println("Starting server...")
+	fmt.Println("Starting server at Port 8080...")
+	log.Fatal(http.ListenAndServe(":8080", nil))
 	log.Fatal(http.ListenAndServe(":8080", nil))
 
 	fmt.Println("...Stopping server")
